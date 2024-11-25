b5c9d1cf880d241fd029a56a509b84cd14aa6a91
Devagya Jangid <devagya.jangid26@gmail.com>
2024-11-25 14:55:51 +0530

    filter dropdown

--- tests/traceability_reports/issueTypeView/TC2-1_filterDropdown.spec.js
+++ tests/traceability_reports/issueTypeView/TC2-1_filterDropdown.spec.js
@@ -2,6 +2,8 @@ const { test } = require("@playwright/test");
 import { utils } from "../utils";
 
 test("TC2-1: Filter Dropdown", async ({page}) => {
+    test.setTimeout(40000);
     await utils.login(page);
+    await utils.filter(page);
     await page.pause();
 })
--- tests/traceability_reports/linkTypeView/TC2-1_filterDropdown.spec.js
+++ tests/traceability_reports/linkTypeView/TC2-1_filterDropdown.spec.js
@@ -2,6 +2,7 @@ const { test } = require("@playwright/test");
 import { utils } from "../utils";
 
 test("TC2-1: Filter Dropdown", async ({page}) => {
+    test.setTimeout(40000);
     await utils.login(page);
     await utils.tpFilter(page);
     await page.pause();
